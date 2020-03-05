dcc29572be4961f604e3acf2aab78c4796768c1f
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-05 17:57:43 +0000

    updated the application layout to display the login/signup button or loguot button if the person is logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to '/', notice: 'Account created successfully'
     else
-      redirect_to '/signup'
+      # redirect_to '/signup'
+      flash[:error] = 'An error occured!'
+      render 'new'
   end
   private
     def user_params
